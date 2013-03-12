f9d29c5bd7aca13d8c7570d6ca99584ce339b34c
Austin Chen <akrolsmir@gmail.com>
2013-03-12 13:32:44 -0700

    Slight tweaks to AI method visibility

--- network/AI.java
+++ network/AI.java
@@ -9,11 +9,11 @@ public class AI {
 	 * for a given move returning a value between -1.0 and 1.0 signifying
 	 * the strength of the given move.
 	 * 
-	 * @param color the turn of the current player(determined by color)
+	 * @param color the turn of the current player (determined by color)
 	 * @param board the current state of the board
 	 * @return a double signifying the strength of a move
 	 */
-	double eval(int color, int[][]board){
+	public double eval(int color, int[][]board){
 		return 0;
 	}
 	
@@ -21,11 +21,11 @@ public class AI {
 	 * bestMove() returns the strongest move for the given player using 
 	 * our evaluation function.
 	 * 
-	 * @param color the turn of the current player(determined by color)
+	 * @param color the turn of the current player (determined by color)
 	 * @param board the current state of the board
 	 * @return a move with the strongest score using our evaluation function
 	 */
-	Move bestMove(int color, int[][]board){
+	public Move bestMove(int color, int[][]board){
 		return null;
 	}
 	
