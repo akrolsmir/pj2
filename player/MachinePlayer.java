148fd45223d17693b71ceabbcef11ea9ec656a3f
Austin Chen <akrolsmir@gmail.com>
2013-03-12 22:14:56 -0700

    Begin implementing MachinePlayer logic

--- player/MachinePlayer.java
+++ player/MachinePlayer.java
@@ -2,26 +2,41 @@
 
 package player;
 
+import ai.AI;
+import ai.Helper;
+
 /**
  *  An implementation of an automatic Network player.  Keeps track of moves
  *  made by both players.  Can select a move for itself.
  */
 public class MachinePlayer extends Player {
+	int[][] board;
+	int color, opponentColor;
 
   // Creates a machine player with the given color.  Color is either 0 (black)
   // or 1 (white).  (White has the first move.)
   public MachinePlayer(int color) {
+	  this(color, 1);
   }
 
   // Creates a machine player with the given color and search depth.  Color is
   // either 0 (black) or 1 (white).  (White has the first move.)
-  public MachinePlayer(int color, int searchDepth) {
-  }
+	public MachinePlayer(int color, int searchDepth) {
+		if (color == 0) {
+			this.color = Helper.BLACK;
+			this.opponentColor = Helper.WHITE;
+		}
+		else {
+			this.color = Helper.WHITE;
+			this.opponentColor = Helper.BLACK;
+		}
+		//TODO do something with searchDepth
+	}
 
   // Returns a new move by "this" player.  Internally records the move (updates
   // the internal game board) as a move by "this" player.
   public Move chooseMove() {
-    return new Move();
+    return AI.bestMove(color, board);
   } 
 
   // If the Move m is legal, records the move as a move by the opponent
@@ -29,7 +44,11 @@ public class MachinePlayer extends Player {
   // illegal, returns false without modifying the internal state of "this"
   // player.  This method allows your opponents to inform you of their moves.
   public boolean opponentMove(Move m) {
-    return false;
+    if(!Helper.isValid(opponentColor, m, board)){
+    	return false;
+    }
+    //TODO update board
+    return true;
   }
 
   // If the Move m is legal, records the move as a move by "this" player
@@ -37,8 +56,12 @@ public class MachinePlayer extends Player {
   // illegal, returns false without modifying the internal state of "this"
   // player.  This method is used to help set up "Network problems" for your
   // player to solve.
-  public boolean forceMove(Move m) {
-    return false;
-  }
+	public boolean forceMove(Move m) {
+		if (!Helper.isValid(color, m, board)) {
+			return false;
+		}
+		//TODO update board
+		return true;
+	}
 
 }
